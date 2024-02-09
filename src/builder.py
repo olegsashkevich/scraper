9454f0a75d12e956f3298ce02f67f790ef77ad94
goftok <goftok46@gmail.com>
2024-02-09 18:34:14 +0100

    Integrated reusable code blocks

--- src/builder.py
+++ src/builder.py
@@ -2,3 +2,10 @@
 from web3._utils.caching import (
     generate_cache_key,
 )
+class TransactionIndexingInProgress(Web3RPCError):
+    """
+    Raised when a transaction receipt is not yet available due to transaction indexing
+    still being in progress.
+    """
+
+
