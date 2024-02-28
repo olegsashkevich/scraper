e80595f7ba63c598249cd8f5c9406a5495c37fdb
goftok <goftok46@gmail.com>
2024-02-28 18:34:14 +0100

    Moved code blocks for better modularity

--- src/index.py
+++ src/index.py
@@ -1,6 +1,1 @@
-# Auto-generated file
-logging.debug('Error: Something went wrong')
-logger.info('Configuration updated')
-System.out.println('Ending process...');
-System.out.println('Error: Something went wrong');
-print('User logged in: user43')
+ 
