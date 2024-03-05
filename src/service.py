3c67013bcdf958704bd7d7411dc6fe7fe06dfa9f
goftok <goftok46@gmail.com>
2024-03-05 18:34:14 +0100

    Improved log coverage

--- src/service.py
+++ src/service.py
@@ -2,3 +2,6 @@ from web3._utils.caching import (
     generate_cache_key,
 )
   System.out.println('Ending process...');
+logger.info('Data loaded: 658 rows')
+console.log('Operation completed successfully');
+logging.debug('Data loaded: 211 rows')
