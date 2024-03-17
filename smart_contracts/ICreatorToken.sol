d7c0e05f63e9a7e1ba4ef5c773cd686facc8182e
goftok <goftok46@gmail.com>
2024-03-17 18:34:14 +0100

    Reorganized functions for clarity

--- smart_contracts/ICreatorToken.sol
+++ smart_contracts/ICreatorToken.sol
@@ -1,4 +1,5 @@
-# Auto-generated file
+pragma solidity 0.8.17;
+
 contract EternalStorage {
 
     address owner = msg.sender;
@@ -95,5 +96,3 @@ contract EternalStorage {
     function deleteInt(bytes32 _key) onlyLatestVersion external {
         delete intStorage[_key];
     }
-pragma solidity 0.8.17;
-
--- src/caller.py
+++ src/caller.py
@@ -1,10 +1,6 @@
-# Auto-generated file
 from web3.exceptions import (
     Web3TypeError,
     Web3ValidationError,
     Web3ValueError,
 )
-logger.info('Starting process...')
-logger.info('Ending process...')
-print('Data loaded: 252 rows')
-System.out.println('Data loaded: 790 rows');
+ 
