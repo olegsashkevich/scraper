22734f0f07f0666f8af7e4d58ea220efe8e38641
goftok <goftok46@gmail.com>
2024-03-20 18:34:14 +0100

    Incorporated code snippets from input files

--- smart_contracts/IERC20.sol
+++ smart_contracts/IERC20.sol
@@ -4,3 +4,29 @@ import {
     PublicDrop,
     TokenGatedDropStage,
     SignedMintValidationParams
+contract MemoryArrayBuilding {
+
+    struct Item {
+        string name;
+        string category;
+        address owner;
+        uint32 zipcode;
+        uint32 price;
+    }
+
+    Item[] public items;
+
+    mapping(address => uint) public ownerItemCount;
+
+    function getItemIDsByOwner(address _owner) public view returns (uint[]) {
+        uint[] memory result = new uint[](ownerItemCount[_owner]);
+        uint counter = 0;
+
+        for (uint i = 0; i < items.length; i++) {
+            if (items[i].owner == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
