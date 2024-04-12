b9df6f2f287679874d21a0a4741bc16548781e86
goftok <goftok46@gmail.com>
2024-04-12 18:34:14 +0200

    Refactored file structure

--- src/module.py
+++ src/module.py
@@ -1,9 +1,1 @@
- def _validate_subscription_fields(response: RPCResponse) -> None:
-    params = response["params"]
-    subscription = params["subscription"]
-    if not isinstance(subscription, str) and not len(subscription) == 34:
-        _raise_bad_response_format(
-            response, "eth_subscription 'params' must include a 'subscription' field."
-        )
-
-
+ 
