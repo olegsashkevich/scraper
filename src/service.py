99deb46627850b45dd38d2c5265369ea49893414
goftok <goftok46@gmail.com>
2024-04-12 18:34:14 +0200

    Refactored file structure

--- src/service.py
+++ src/service.py
@@ -2,7 +2,4 @@ from web3._utils.caching import (
     generate_cache_key,
 )
   System.out.println('Ending process...');
-logger.info('Data loaded: 658 rows')
-console.log('Operation completed successfully');
-logging.debug('Data loaded: 211 rows')
-console.log('Error: Something went wrong');
+ 
--- src/testing.py
+++ src/testing.py
@@ -1,12 +1,8 @@
-# Auto-generated file
+from web3._utils.compat import (
+    Self,
+)
 from web3._utils.encoding import (
     hex_encode_abi_type,
     to_hex,
     to_json,
-)
-logging.debug('User logged in: user12')
-console.log('User logged in: user35');
-print('Configuration updated')
-from web3._utils.compat import (
-    Self,
 )
