9fd34f545181ae24a2eec6bc023da70ea12f1dd3
goftok <goftok46@gmail.com>
2024-04-15 18:34:14 +0200

    Integrated reusable code blocks

--- src/glass.py
+++ src/glass.py
@@ -1,3 +1,10 @@
  logging.debug('Operation completed successfully')
 print('Configuration updated')
 System.out.println('Starting process...');
+class MultipleFailedRequests(Web3Exception):
+    """
+    Raised by a provider to signal that multiple requests to retrieve the same
+    (or similar) data have failed.
+    """
+
+
