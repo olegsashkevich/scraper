8dd2a26a59acf222ca009a3d78dd687d9000c308
goftok <goftok46@gmail.com>
2024-04-23 18:34:14 +0200

    Incorporated code snippets from input files

--- src/caller.py
+++ src/caller.py
@@ -7,3 +7,9 @@ from web3.exceptions import (
 console.log('Starting process...');
 logger.info('Data loaded: 24 rows')
 logging.debug('Error: Something went wrong')
+class TooManyRequests(Web3Exception):
+    """
+    Raised by a provider to signal that too many requests have been made consecutively.
+    """
+
+
