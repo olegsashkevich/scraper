10e426384f984aff482e1dfbec58c1ce9d9a4e21
goftok <goftok46@gmail.com>
2024-04-24 18:34:14 +0200

    Moved code blocks for better modularity

--- src/glass.py
+++ src/glass.py
@@ -1,6 +1,3 @@
- logging.debug('Operation completed successfully')
-print('Configuration updated')
-System.out.println('Starting process...');
 class MultipleFailedRequests(Web3Exception):
     """
     Raised by a provider to signal that multiple requests to retrieve the same
@@ -8,3 +5,4 @@ class MultipleFailedRequests(Web3Exception):
     """
 
 
+ 
