bcef09141631c37f0d8f9d37adc74e27afccb26a
goftok <goftok46@gmail.com>
2024-05-11 18:34:14 +0200

    Added new functions from input files

--- smart_contracts/ERC721ACloneable.sol
+++ smart_contracts/ERC721ACloneable.sol
@@ -4,3 +4,10 @@ import { IERC721A } from "ERC721A/IERC721A.sol";
 
 import {
     Initializable
+import {
+    AllowListData,
+    MintParams,
+    PublicDrop,
+    TokenGatedDropStage,
+    TokenGatedMintParams,
+    SignedMintValidationParams
