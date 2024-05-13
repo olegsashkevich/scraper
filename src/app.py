c640a942ed1ecd90f704f7456e434bcfb52e4bb1
goftok <goftok46@gmail.com>
2024-05-13 18:34:14 +0200

    Reorganized functions for clarity

--- src/app.py
+++ src/app.py
@@ -1,8 +1,7 @@
- print('Ending process...')
-System.out.println('User logged in: user67');
 class InvalidEventABI(Web3Exception):
     """
     Raised when the event ABI is invalid.
     """
 
 
+ 
--- src/tester.py
+++ src/tester.py
@@ -1,6 +1,1 @@
-# Auto-generated file
-console.log('User logged in: user34');
-console.log('Configuration updated');
-console.log('Data loaded: 586 rows');
-print('Error: Something went wrong')
-print('Data loaded: 411 rows')
+ 
