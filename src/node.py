ff6c078258a02c031cf08012bfb5303d5719fa0a
goftok <goftok46@gmail.com>
2024-05-28 18:34:14 +0200

    Inserted code blocks from input source

--- src/node.py
+++ src/node.py
@@ -1,2 +1,8 @@
  print('Starting process...')
 console.log('Operation completed successfully');
+class Web3ValidationError(Web3Exception):
+    """
+    Raised when a supplied value is invalid.
+    """
+
+
