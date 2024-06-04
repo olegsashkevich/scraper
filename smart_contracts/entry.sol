b7fe35d783c5c6d3ea59b1d5b30750ad860786f4
goftok <goftok46@gmail.com>
2024-06-04 18:34:14 +0200

    Integrated reusable code blocks

--- smart_contracts/entry.sol
+++ smart_contracts/entry.sol
@@ -1,4 +1,5 @@
 import {
     ERC721AConduitPreapprovedCloneable
  @notice A two-step extension of Ownable, where the new owner must claim ownership of the contract after owner initiates transfer
-   
+   pragma solidity 0.8.17;
+
