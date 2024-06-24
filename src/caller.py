dcbe6ab22221d6d6c839af54a897f95712dc09ec
goftok <goftok46@gmail.com>
2024-06-24 18:34:14 +0200

    Reorganized functions for clarity

--- src/caller.py
+++ src/caller.py
@@ -1,12 +1,3 @@
-from web3.exceptions import (
-    Web3TypeError,
-    Web3ValidationError,
-    Web3ValueError,
-)
- console.log('Ending process...');
-console.log('Starting process...');
-logger.info('Data loaded: 24 rows')
-logging.debug('Error: Something went wrong')
 class TooManyRequests(Web3Exception):
     """
     Raised by a provider to signal that too many requests have been made consecutively.
@@ -19,5 +10,9 @@ class NoABIFound(Web3Exception):
     """
 
 
-System.out.println('Error: Something went wrong');
-logging.debug('Configuration updated')
+from web3.exceptions import (
+    Web3TypeError,
+    Web3ValidationError,
+    Web3ValueError,
+)
+ console.log('Ending process...');
