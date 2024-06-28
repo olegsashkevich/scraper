7cfed7f89e40cd9ab38b96855cf8297cba057762
goftok <goftok46@gmail.com>
2024-06-28 18:34:14 +0200

    Added new functions from input files

--- src/glass.py
+++ src/glass.py
@@ -6,4 +6,18 @@ class MultipleFailedRequests(Web3Exception):
 
 
  console.log('Starting process...');
-  
+  def _set_mungers(
+    mungers: Optional[Sequence[Munger]], is_property: bool
+) -> Sequence[Any]:
+    if is_property and mungers:
+        raise Web3ValidationError("Mungers cannot be used with a property.")
+
+    return (
+        mungers
+        if mungers
+        else [default_munger]
+        if is_property
+        else [default_root_munger]
+    )
+
+
