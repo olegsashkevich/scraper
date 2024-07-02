10c34c40e798925f70e160a9cf6263aa97df639b
goftok <goftok46@gmail.com>
2024-07-02 18:34:14 +0200

    Integrated reusable code blocks

--- smart_contracts/main.sol
+++ smart_contracts/main.sol
@@ -3,3 +3,5 @@
 import {
     Initializable
 @notice A two-step extension of Ownable, where the new owner must claim ownership of the contract after owner initiates transfer
+import {
+    ISeaDropTokenContractMetadata
