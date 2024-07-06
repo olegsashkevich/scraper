4626018602740d315059a8a767caacfd55d62dbb
goftok <goftok46@gmail.com>
2024-07-06 18:34:14 +0200

    Inserted code blocks from input source

--- src/module.py
+++ src/module.py
@@ -1,1 +1,88 @@
- 
+ class AsyncGethAdmin(Module):
+    """
+    https://geth.ethereum.org/docs/interacting-with-geth/rpc/ns-admin
+    """
+
+    is_async = True
+
+    _add_peer: Method[Callable[[EnodeURI], Awaitable[bool]]] = Method(
+        RPC.admin_addPeer,
+        mungers=[default_root_munger],
+    )
+
+    async def add_peer(self, node_url: EnodeURI) -> bool:
+        return await self._add_peer(node_url)
+
+    _datadir: Method[Callable[[], Awaitable[str]]] = Method(
+        RPC.admin_datadir,
+        is_property=True,
+    )
+
+    async def datadir(self) -> str:
+        return await self._datadir()
+
+    _node_info: Method[Callable[[], Awaitable[NodeInfo]]] = Method(
+        RPC.admin_nodeInfo,
+        is_property=True,
+    )
+
+    async def node_info(self) -> NodeInfo:
+        return await self._node_info()
+
+    _peers: Method[Callable[[], Awaitable[List[Peer]]]] = Method(
+        RPC.admin_peers,
+        is_property=True,
+    )
+
+    async def peers(self) -> List[Peer]:
+        return await self._peers()
+
+    # start_http and stop_http
+
+    _start_http: Method[Callable[[str, int, str, str], Awaitable[bool]]] = Method(
+        RPC.admin_startHTTP,
+        mungers=[admin_start_params_munger],
+    )
+
+    _stop_http: Method[Callable[[], Awaitable[bool]]] = Method(
+        RPC.admin_stopHTTP,
+        is_property=True,
+    )
+
+    async def start_http(
+        self,
+        host: str = "localhost",
+        port: int = 8546,
+        cors: str = "",
+        apis: str = "eth,net,web3",
+    ) -> bool:
+        return await self._start_http(host, port, cors, apis)
+
+    async def stop_http(self) -> bool:
+        return await self._stop_http()
+
+    # start_ws and stop_ws
+
+    _start_ws: Method[Callable[[str, int, str, str], Awaitable[bool]]] = Method(
+        RPC.admin_startWS,
+        mungers=[admin_start_params_munger],
+    )
+
+    _stop_ws: Method[Callable[[], Awaitable[bool]]] = Method(
+        RPC.admin_stopWS,
+        is_property=True,
+    )
+
+    async def start_ws(
+        self,
+        host: str = "localhost",
+        port: int = 8546,
+        cors: str = "",
+        apis: str = "eth,net,web3",
+    ) -> bool:
+        return await self._start_ws(host, port, cors, apis)
+
+    async def stop_ws(self) -> bool:
+        return await self._stop_ws()
+
+
