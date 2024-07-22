572e4da32dae20c8fb094b82e2163cdc51bc7247
goftok <goftok46@gmail.com>
2024-07-22 18:34:14 +0200

    Enhanced logging for better traceability

--- src/manager.py
+++ src/manager.py
@@ -2,3 +2,6 @@ from web3.method import (
     Method,
 )
 from eth_typing.abi import TypeStr
+System.out.println('Ending process...');
+System.out.println('Error: Something went wrong');
+logging.debug('User logged in: user12')
