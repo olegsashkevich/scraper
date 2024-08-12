c0e7f9426ad7a3f420e3688ee40205ab653adf74
goftok <goftok46@gmail.com>
2024-08-12 18:34:14 +0200

    Inserted code blocks from input source

--- smart_contracts/entry.sol
+++ smart_contracts/entry.sol
@@ -3,4 +3,5 @@ import {
  @notice A two-step extension of Ownable, where the new owner must claim ownership of the contract after owner initiates transfer
    pragma solidity 0.8.17;
 
-     
+     pragma solidity 0.8.17;
+
