82e6357d17db44b03c50ba2b9ea09212b7dc2f22
goftok <goftok46@gmail.com>
2024-08-16 18:34:14 +0200

    Integrated reusable code blocks

--- src/manager.py
+++ src/manager.py
@@ -2,3 +2,7 @@ from eth_typing.abi import TypeStr
 from web3.method import (
     Method,
 )
+class BlockNotFound(Web3RPCError):
+    """
+    Raised when the block id used to look up a block in a jsonrpc call cannot be found.
+    """
