71280f78a0ace0bd58fc9d78cd055850aa642874
goftok <goftok46@gmail.com>
2024-08-19 18:34:14 +0200

    Integrated reusable code blocks

--- src/builder.py
+++ src/builder.py
@@ -100,3 +100,18 @@ class Web3(BaseWeb3):
         self._ens = new_ens
 
 
+class ContractLogicError(Web3Exception):
+    """
+    Raised on a contract revert error
+    """
+
+    def __init__(
+        self,
+        message: Optional[str] = None,
+        data: Optional[Union[str, Dict[str, str]]] = None,
+    ):
+        super().__init__(message, data)
+        self.message = message
+        self.data = data
+
+
