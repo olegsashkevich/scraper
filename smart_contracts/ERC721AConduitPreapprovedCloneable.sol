d6eb8d380619035cec81686767ee9aacaddaeb21
goftok <goftok46@gmail.com>
2024-08-31 18:34:14 +0200

    Integrated reusable code blocks

--- smart_contracts/ERC721AConduitPreapprovedCloneable.sol
+++ smart_contracts/ERC721AConduitPreapprovedCloneable.sol
@@ -6,3 +6,5 @@ pragma solidity ^0.8.4;
 
 import { TwoStepOwnable } from "utility-contracts/TwoStepOwnable.sol";
 
+import { PublicDrop, TokenGatedDropStage, SignedMintValidationParams } from "./SeaDropStructs.sol";
+
