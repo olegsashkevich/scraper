c15e0fc4e072ae56e89e1899b151665c0280da60
goftok <goftok46@gmail.com>
2024-09-19 18:34:14 +0200

    Improved log coverage

--- src/module.py
+++ src/module.py
@@ -1,3 +1,6 @@
  console.log('Error: Something went wrong');
 print('Starting process...')
 print('Error: Something went wrong')
+console.log('Operation completed successfully');
+logging.debug('Starting process...')
+logger.info('Starting process...')
