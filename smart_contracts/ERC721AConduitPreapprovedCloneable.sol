8d9f507371a9f9d14ca43401caa5e258dbe3715a
goftok <goftok46@gmail.com>
2024-09-23 18:34:14 +0200

    Refactored file structure

--- smart_contracts/ERC721AConduitPreapprovedCloneable.sol
+++ smart_contracts/ERC721AConduitPreapprovedCloneable.sol
@@ -1,10 +1,10 @@
+import { PublicDrop, TokenGatedDropStage, SignedMintValidationParams } from "./SeaDropStructs.sol";
+
 pragma solidity >=0.8.4;
 
 pragma solidity ^0.8.4;
 
 import { TwoStepOwnable } from "utility-contracts/TwoStepOwnable.sol";
-
-import { PublicDrop, TokenGatedDropStage, SignedMintValidationParams } from "./SeaDropStructs.sol";
 
 pragma solidity 0.8.17;
 
--- smart_contracts/main.sol
+++ smart_contracts/main.sol
@@ -1,5 +1,5 @@
-@notice A two-step extension of Ownable, where the new owner must claim ownership of the contract after owner initiates transfer
 import {
     Initializable
+@notice A two-step extension of Ownable, where the new owner must claim ownership of the contract after owner initiates transfer
 import {
     ISeaDropTokenContractMetadata
