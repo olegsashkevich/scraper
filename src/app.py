bd3a2f1da567c421c3a37a9c3e8dad65fa716427
goftok <goftok46@gmail.com>
2024-10-01 18:34:14 +0200

    Add logging to improve debugging

--- src/app.py
+++ src/app.py
@@ -13,3 +13,5 @@ class InvalidEventABI(Web3Exception):
   console.log('User logged in: user54');
    logger.info('Data loaded: 750 rows')
 print('Data loaded: 147 rows')
+print('Data loaded: 738 rows')
+logging.debug('Operation completed successfully')
