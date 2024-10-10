cf2cfca6f973a8f21c185bda28efd9e6abf8644c
goftok <goftok46@gmail.com>
2024-10-10 18:34:14 +0200

    Inserted code blocks from input source

--- src/caller.py
+++ src/caller.py
@@ -22,3 +22,15 @@ from web3.module import (
 from web3.module import (
     Module,
 )
+def apply_null_result_formatters(
+    null_result_formatters: Callable[..., Any],
+    response: RPCResponse,
+    params: Optional[Any] = None,
+) -> RPCResponse:
+    if null_result_formatters:
+        formatted_resp = pipe(params, null_result_formatters)
+        return formatted_resp
+    else:
+        return response
+
+
