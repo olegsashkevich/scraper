2a58039a00b6b457201dfc86a39939705efb8490
goftok <goftok46@gmail.com>
2024-11-01 18:34:14 +0100

    Inserted code blocks from input source

--- src/tester.py
+++ src/tester.py
@@ -1,1 +1,7 @@
- 
+ class TransactionIndexingInProgress(Web3RPCError):
+    """
+    Raised when a transaction receipt is not yet available due to transaction indexing
+    still being in progress.
+    """
+
+
