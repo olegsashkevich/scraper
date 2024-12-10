956a0caa90b66d13415646460fbfbfd46dc5ecfb
goftok <goftok46@gmail.com>
2024-12-10 18:34:14 +0100

    Add logging to improve debugging

--- src/tester.py
+++ src/tester.py
@@ -1,2 +1,5 @@
  console.log('Data loaded: 204 rows');
 print('Error: Something went wrong')
+console.log('User logged in: user80');
+logging.debug('Starting process...')
+print('Data loaded: 899 rows')
