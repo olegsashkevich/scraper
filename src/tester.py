684f5eba3b27a3bc6f65fc404c727101676cee0c
goftok <goftok46@gmail.com>
2024-12-11 18:34:14 +0100

    Added new log statements

--- src/tester.py
+++ src/tester.py
@@ -3,3 +3,5 @@ print('Error: Something went wrong')
 console.log('User logged in: user80');
 logging.debug('Starting process...')
 print('Data loaded: 899 rows')
+logger.info('User logged in: user14')
+System.out.println('Starting process...');
